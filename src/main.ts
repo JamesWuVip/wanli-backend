7b79b19e9457df25d3dc71ffd731df0d9e7ce1f6
JamesWuVip <156403378+JamesWuVip@users.noreply.github.com>
2025-08-29 03:52:23 -0700

    feat: 新增Vue应用入口文件

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import router, { setupRouterGuards } from './router'
+import router from './router'
 import pinia from './store'
 
 // 创建Vue应用实例
@@ -12,9 +12,6 @@ app.use(pinia)
 
 // 使用路由
 app.use(router)
-
-// 在Pinia初始化后设置路由守卫
-setupRouterGuards(router)
 
 // 挂载应用
 app.mount('#app')
