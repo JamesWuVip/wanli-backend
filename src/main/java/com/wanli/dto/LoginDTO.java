6b4c21f8a3e1fc490d1157931ff34d88fb45df53
JamesWuVip <156403378+JamesWuVip@users.noreply.github.com>
2025-09-01 08:21:10 -0700

    feat(sp1): 添加用户登录请求DTO

--- src/main/java/com/wanli/dto/LoginDTO.java
+++ src/main/java/com/wanli/dto/LoginDTO.java
@@ -5,35 +5,23 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 /**
- * 登录请求DTO.
+ * 用户登录请求数据传输对象.
  *
- * <p>用于接收用户登录请求的手机号和密码信息，包含数据验证规则。</p>
+ * <p>用于封装用户登录时提交的手机号和密码信息，包含相应的验证规则。</p>
  *
  * @author JamesWu
  * @since 1.0.0
  */
-public class LoginDTO {
-
-    /** 手机号长度. */
-    private static final int PHONE_LENGTH = 11;
-
-    /** 密码最小长度. */
-    private static final int MIN_PASSWORD_LENGTH = 6;
-
-    /** 密码最大长度. */
-    private static final int MAX_PASSWORD_LENGTH = 100;
+public final class LoginDTO {
 
     /** 手机号. */
     @NotBlank(message = "手机号不能为空")
-    @Size(min = PHONE_LENGTH, max = PHONE_LENGTH,
-          message = "手机号必须为11位数字")
     @Pattern(regexp = "^1[3-9]\\d{9}$", message = "手机号格式不正确")
-    private String phone;
+    private String phoneNumber;
 
     /** 密码. */
     @NotBlank(message = "密码不能为空")
-    @Size(min = MIN_PASSWORD_LENGTH, max = MAX_PASSWORD_LENGTH,
-          message = "密码长度必须在6-100个字符之间")
+    @Size(min = 6, max = 20, message = "密码长度必须在6-20位之间")
     private String password;
 
     /**
@@ -43,38 +31,38 @@ public class LoginDTO {
     }
 
     /**
-     * 带参数的构造函数.
+     * 全参构造函数.
      *
-     * @param userPhone 手机号.
-     * @param userPassword 密码.
+     * @param phoneNumber 手机号
+     * @param password 密码
      */
-    public LoginDTO(final String userPhone, final String userPassword) {
-        this.phone = userPhone;
-        this.password = userPassword;
+    public LoginDTO(final String phoneNumber, final String password) {
+        this.phoneNumber = phoneNumber;
+        this.password = password;
     }
 
     /**
      * 获取手机号.
      *
-     * @return 手机号.
+     * @return 手机号
      */
-    public String getPhone() {
-        return phone;
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 
     /**
      * 设置手机号.
      *
-     * @param userPhone 手机号.
+     * @param phoneNumber 手机号
      */
-    public void setPhone(final String userPhone) {
-        this.phone = userPhone;
+    public void setPhoneNumber(final String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
     /**
      * 获取密码.
      *
-     * @return 密码.
+     * @return 密码
      */
     public String getPassword() {
         return password;
@@ -83,24 +71,24 @@ public class LoginDTO {
     /**
      * 设置密码.
      *
-     * @param userPassword 密码.
+     * @param password 密码
      */
-    public void setPassword(final String userPassword) {
-        this.password = userPassword;
+    public void setPassword(final String password) {
+        this.password = password;
     }
 
     /**
-     * 返回对象的字符串表示.
+     * 重写toString方法.
      *
-     * <p>为了安全考虑，密码字段显示为[PROTECTED]。</p>.
+     * <p>为了安全考虑，不输出密码信息。</p>
      *
-     * @return 对象的字符串表示.
+     * @return 字符串表示
      */
     @Override
     public String toString() {
         return "LoginDTO{"
-                + "phone='" + phone
-                + "', password='[PROTECTED]'"
-                + '}';
+                + "phoneNumber='" + phoneNumber + "'"
+                + ", password='[PROTECTED]'"
+                + "}";
     }
 }
